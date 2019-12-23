138892c972e32212925f5cb81f1959cc08b7e38e
trungpq <trungpq.ml@gmail.com>
2019-12-23 14:17:05 +0700

    Finish Array List

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const bubble_sort = require("./src/bubble_sort")
 const insertion_sort = require("./src/insertion_sort")
 const merge_sort = require("./src/merge_sort")
+const ArrayList = require("./src/array_list")
 
-var nums = [10, 5, 3, 8, 13, 18, 29, 2, 6, 4, 7, 9, 1];
+// var nums = [10, 5, 3, 8, 13, 18, 29, 2, 6, 4, 7, 9, 1];
 // var nums = [1, 2, 3, 4, 5]
-console.log(`Before sorting: ${nums.join(" - ")}`)
+// console.log(`Before sorting: ${nums.join(" - ")}`)
 
 // nums = bubble_sort(nums)
 // nums = insertion_sort(nums)
-nums = merge_sort(nums, 0, nums.length - 1)
+// nums = merge_sort(nums, 0, nums.length - 1)
 
-console.log(`After sorting: ${nums.join(" - ")}`)
+// console.log(`After sorting: ${nums.join(" - ")}`)
+
+let array = new ArrayList()
+
+console.log(array.length)
---
+++ src/array_list.js
@@ -0,0 +1,34 @@
+class ArrayList {
+    constructor() {
+        this.length = 0
+        this.data = {}
+    }
+
+    push(value) {
+        this.data[this.length] = value
+        this.length++;
+    }
+
+    pop() {
+        return this.delete(this.length - 1)
+    }
+    get(index) {
+        return this.data[index]
+    }
+
+    delete(index) {
+        const ans = this.data[index]
+        this._collapseTo(index)
+        return ans
+    }
+
+    _collapseTo(index) {
+        for (let i = index; i < array.length; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+        delete this.data[this.length - 1]
+        this.length--
+    }
+}
+
+module.exports = ArrayList
